0892dc10371754656300c798d3d96f936ad3000a
skjjain <saurabh.sgsits@gmail.com>
2014-03-07 00:15:51 +0530

    Sprite with Changing Frame

--- killthemall/src/com/torrent/killthemall/Sprite.java
+++ killthemall/src/com/torrent/killthemall/Sprite.java
@@ -1,5 +1,7 @@
 package com.torrent.killthemall;
 
+import java.util.Random;
+
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Rect;
@@ -12,37 +14,68 @@ public class Sprite {
 	private int y = 0;
 	private int height = 0;
 	private int width = 0;
-	private int xSpeed = 5;
+	private int xSpeed;
+	private int ySpeed;
 	GameView gameView;
 	Bitmap bmp;
 	private int currentRow = 1;
 	private int currentCol = 0;
 	
+	private int UP = 3;
+	private int DOWN = 0;
+	private int LEFT = 1;
+	private int RIGHT = 2;
+
 	public Sprite(GameView gameView, Bitmap bmp) {
 		this.gameView = gameView;
 		this.bmp = bmp;
 		this.width = bmp.getWidth() / BITMAP_COLUMN;
 		this.height = bmp.getHeight() / BITMAP_ROWS;
+		Random rnd = new Random();
+		ySpeed = rnd.nextInt(10) - 5;
+		xSpeed = rnd.nextInt(10) - 5;
 	}
-	
-	public void update(){
-		if(x > gameView.getWidth() - width - xSpeed){
-			//currentRow = 1;
-			xSpeed =  -5;
+
+	public void update() {
+		if (x > gameView.getWidth() - width - xSpeed || x + xSpeed < 0) {
+			xSpeed = -xSpeed;
 		}
-		
-		if(x + xSpeed < 0){
-			//currentRow = 2;
-			xSpeed =  5;
-		}
-		
-		currentCol = ++currentCol % BITMAP_COLUMN;
 		x += xSpeed;
+
+		if (y > gameView.getHeight() - height - ySpeed || y + ySpeed < 0) {
+			ySpeed = -ySpeed;
+		}
+		y += ySpeed;
+
+		currentCol = ++currentCol % BITMAP_COLUMN;
+		updateCurrentRow();
+
 	}
 	
-	public void onDraw(Canvas canvas){
+	public void updateCurrentRow() {
+		double angel = Math.toDegrees(Math.atan2(ySpeed, xSpeed)) + 180;
+
+		if (angel > 315 || angel <= 45) {
+			currentRow = LEFT;
+			// println("go right when " +angel);
+		} else if (angel > 45 && angel <= 135) {
+			currentRow = UP;
+			// println("go up when " +angel);
+
+		} else if (angel > 135 && angel <= 225) {
+			currentRow = RIGHT;
+			// println("go left when " +angel);
+
+		} else if (angel > 225 && angel <= 315) {
+			currentRow = DOWN;
+			// println("go down when " +angel);
+
+		}
+	}
+
+	public void onDraw(Canvas canvas) {
 		update();
-		int srcY  = height * currentRow;
+		int srcY = height * currentRow;
 		int srcX = width * currentCol;
 		Rect src = new Rect(srcX, srcY, srcX + width, srcY + height);
 		Rect dst = new Rect(x, y, x + width, y + height);
