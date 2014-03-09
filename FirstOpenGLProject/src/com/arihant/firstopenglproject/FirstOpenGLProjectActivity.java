43999634a06435e4013b663a6db3c8dd5f8e6fe6
Saurabh Jain <saurabh.jain@buzzyears.com>
2014-03-09 22:45:23 +0530

    Open GL Project

--- FirstOpenGLProject/src/com/arihant/firstopenglproject/FirstOpenGLProjectActivity.java
+++ FirstOpenGLProject/src/com/arihant/firstopenglproject/FirstOpenGLProjectActivity.java
@@ -10,86 +10,61 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class FirstOpenGLProjectActivity extends Activity {
-    /**
-     * Hold a reference to our GLSurfaceView
-     */
-    private GLSurfaceView glSurfaceView;
-    private boolean rendererSet = false;
+	/**
+	 * Hold a reference to our GLSurfaceView
+	 */
+	private GLSurfaceView glSurfaceView;
+	private boolean rendererSet = false;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        glSurfaceView = new GLSurfaceView(this);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		glSurfaceView = new GLSurfaceView(this);
+		// Check if the system supports OpenGL ES 2.0.
+		if (supportsES2()) {
+			// Request an OpenGL ES 2.0 compatible context.
+			glSurfaceView.setEGLContextClientVersion(2);
+			// Assign our renderer.
+			glSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+			glSurfaceView.setRenderer(new FirstOpenGLProjectRenderer());
+			rendererSet = true;
+		} else {
+			Toast.makeText(this, "This device does not support OpenGL ES 2.0.",
+					Toast.LENGTH_LONG).show();
+			return;
+		}
 
-        // Check if the system supports OpenGL ES 2.0.
-        final ActivityManager activityManager =
-            (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
- 
-        final ConfigurationInfo configurationInfo =
-            activityManager.getDeviceConfigurationInfo();
-        /*
-		
-        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
-         */
-        // Even though the latest emulator supports OpenGL ES 2.0,
-        // it has a bug where it doesn't set the reqGlEsVersion so
-        // the above check doesn't work. The below will detect if the
-        // app is running on an emulator, and assume that it supports
-        // OpenGL ES 2.0.
-        final boolean supportsEs2 =
-            configurationInfo.reqGlEsVersion >= 0x20000
-                || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
-                 && (Build.FINGERPRINT.startsWith("generic")
-                  || Build.FINGERPRINT.startsWith("unknown")
-                  || Build.MODEL.contains("google_sdk")
-                  || Build.MODEL.contains("Emulator")
-                  || Build.MODEL.contains("Android SDK built for x86")));
+		setContentView(glSurfaceView);
+	}
 
-        if (supportsEs2) {
-            // Request an OpenGL ES 2.0 compatible context.
-            glSurfaceView.setEGLContextClientVersion(2);
+	@Override
+	protected void onPause() {
+		super.onPause();
 
-            // Assign our renderer.
-            glSurfaceView.setEGLConfigChooser(8 , 8, 8, 8, 16, 0);
-            glSurfaceView.setRenderer(new FirstOpenGLProjectRenderer());
-            rendererSet = true;
-        } else {
-            /*
-             * This is where you could create an OpenGL ES 1.x compatible
-             * renderer if you wanted to support both ES 1 and ES 2. Since we're
-             * not doing anything, the app will crash if the device doesn't
-             * support OpenGL ES 2.0. If we publish on the market, we should
-             * also add the following to AndroidManifest.xml:
-             * 
-             * <uses-feature android:glEsVersion="0x00020000"
-             * android:required="true" />
-             * 
-             * This hides our app from those devices which don't support OpenGL
-             * ES 2.0.
-             */
-            Toast.makeText(this, "This device does not support OpenGL ES 2.0.",
-                Toast.LENGTH_LONG).show();
-            return;
-        }
+		if (rendererSet) {
+			glSurfaceView.onPause();
+		}
+	}
 
-        setContentView(glSurfaceView);
-    }
+	@Override
+	protected void onResume() {
+		super.onResume();
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        
-        if (rendererSet) {
-            glSurfaceView.onPause();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        
-        if (rendererSet) {
-            glSurfaceView.onResume();
-        }
-    }
+		if (rendererSet) {
+			glSurfaceView.onResume();
+		}
+	}
+	
+	private boolean supportsES2() {
+		final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+		final ConfigurationInfo configurationInfo = activityManager
+				.getDeviceConfigurationInfo();
+		return configurationInfo.reqGlEsVersion >= 0x20000
+				|| (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1 && (Build.FINGERPRINT
+						.startsWith("generic")
+						|| Build.FINGERPRINT.startsWith("unknown")
+						|| Build.MODEL.contains("google_sdk")
+						|| Build.MODEL.contains("Emulator") || Build.MODEL
+							.contains("Android SDK built for x86")));
+	}
 }
