ba5c6f2a7d7c9d862b659c3da4057fce8ee82a2b
Saurabh Jain <saurabh.jain@buzzyears.com>
2014-03-22 00:09:25 +0530

    Platformer

---
+++ Platformer/src/com/arihant/platformer/GameActivity.java
@@ -0,0 +1,41 @@
+package com.arihant.platformer;
+
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.BaseGameActivity;
+
+public class GameActivity extends BaseGameActivity {
+
+	public GameActivity() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onCreateResources(
+			OnCreateResourcesCallback pOnCreateResourcesCallback)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPopulateScene(Scene pScene,
+			OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
