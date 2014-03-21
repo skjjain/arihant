29e24f982ecea27d3487306a4dd7deffc91b6922
Saurabh Jain <saurabh.jain@buzzyears.com>
2014-03-22 01:01:01 +0530

    Created Engine Options

--- Platformer/src/com/arihant/platformer/GameActivity.java
+++ Platformer/src/com/arihant/platformer/GameActivity.java
@@ -1,19 +1,35 @@
 package com.arihant.platformer;
 
+import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.WakeLockOptions;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
+import org.andengine.input.touch.controller.MultiTouch;
 import org.andengine.ui.activity.BaseGameActivity;
+
+import android.widget.Toast;
 
 public class GameActivity extends BaseGameActivity {
 
+	private Camera camera;
+	
 	public GameActivity() {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public EngineOptions onCreateEngineOptions() {
-		// TODO Auto-generated method stub
-		return null;
+		camera = new Camera(0, 0, 800, 480);
+		EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(800, 480), camera);
+		engineOptions.getAudioOptions().setNeedsMusic(true).setNeedsSound(true);
+		engineOptions.setWakeLockOptions(WakeLockOptions.SCREEN_ON);
+		engineOptions.getTouchOptions().setNeedsMultiTouch(true);
+		if(!MultiTouch.isSupported(this)){
+			Toast.makeText(this, "Sorry your device does not support multitouch!", Toast.LENGTH_LONG);
+		}
+		return engineOptions;
 	}
 
 	@Override
